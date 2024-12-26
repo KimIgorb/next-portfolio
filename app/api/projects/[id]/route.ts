299ca41d154cb2646f52b8757ad7842309089485
Игорь <2712190@gmail.com>
2024-12-26 21:05:17 +0500

    test

--- app/api/projects/[id]/route.ts
+++ app/api/projects/[id]/route.ts
@@ -1,12 +1,9 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "../../../../prisma/prisma-client";
 
-interface Context {
-  params: { id: string };
-}
 
-export async function GET(request: Request, context: Context) {
-  const { id } = context.params;
+export async function GET(request: NextRequest, { params }: { params: { id: string } }) {
+  const { id } = params
 
   const project = await prisma.projects.findUnique({
     where: { id: parseInt(id) },
