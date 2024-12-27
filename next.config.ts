c7f51786134dda2c307cb376ead90d9b71e9d513
Игорь <2712190@gmail.com>
2024-12-27 21:38:47 +0500

    test

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,27 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/api/:path*", // Это правило применяется ко всем API маршрутам
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*", // Или укажите домен, с которого разрешены запросы
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, PATCH",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
