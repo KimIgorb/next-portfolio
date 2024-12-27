c8a33647229d5c8a8e957d5843cfe68699518810
Игорь <2712190@gmail.com>
2024-12-27 20:45:52 +0500

    test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,6 @@ export default async function RootLayout({
   const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/theme`, {cache: 'no-store'});
   const theme = await response.json()
   
-  
   return (
     <html lang="en" className={theme.mode}>
       <body
